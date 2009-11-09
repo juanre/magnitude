d212b039c2df2cd549ae14fa401d38a0973ad6d9
Juan Reyero <juan@juanreyero.com>
2009-11-09 16:12:50 +0100

    Changed version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='magnitude',
-      version='0.9.3',
+      version='0.9.4',
       description='Python library for computing with numbers with units',
       author='Juan Reyero',
       author_email='juan@juanreyero.com',
